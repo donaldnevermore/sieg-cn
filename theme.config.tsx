6ec5193025a1f3dc60667362defaced8c86d886f
Donald33 Wang <donald33wang@gmail.com>
2023-10-27 15:07:49 +0800

    Add ch1

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>A Student's Introduction to English Grammar</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
